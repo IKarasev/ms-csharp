40b23db9f4dc22ae1ad4e4bc2127fc5c80c27ec2
Ilya Karasev <iakarsev@gmail.com>
2021-06-22 18:07:33 +0300

    Started numbers chapter

---
+++ numbers/Numbers/Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // arifmetics
+            int a = 5;
+            int b = 18;
+            int c = a + b;
+            Console.WriteLine(c);
+
+            Console.ReadKey();
+        }
+    }
+}
